64dabce82f91a7a66a2d845953466f0eeaa97135
Dust1920 <a218208885@gmail.com>
2024-04-05 14:32:50 -0700

    data

--- colors.py
+++ colors.py
@@ -1,2 +1,10 @@
 from matplotlib.colors import ListedColormap
 
+
+color_test = 'orange'
+cmap_test = 'viridis'
+
+region_norte = ['BCN','BCS','SON','CHH','SIN','DUR','COA','NLE','TAM']
+region_sur = ['GRO','OAX','CHP','TAB','CAM','ROO','YUC']
+region_oriente = ['VER','HID', 'PUE','TLA','MEX','CMX','MOR']
+region_occidente = ['NAY','JAL','COL','MIC','ZAC','AGU','SLP','GUA','QUE']
